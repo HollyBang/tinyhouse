2739ac4324376e9e367dece25ea10b184bf07e71
HollyBang <mazafakand@gmail.com>
2020-03-30 23:44:55 +0300

    added seed for database & changed resolvers

--- src/index.ts
+++ src/index.ts
@@ -8,14 +8,12 @@ import { typeDefs, resolvers } from './graphql';
 const mount = async (app: Application) => {
   const db = await connectDatabase();
   const server = new ApolloServer({ typeDefs, resolvers, context: () => ({ db }) });
+
   server.applyMiddleware({ app, path: '/api' });
 
   app.listen(process.env.PORT);
 
   console.log(`[app] : http://localhost:${process.env.PORT}`);
-
-  const listings = await db.listings.find({}).toArray();
-  console.log(listings);
 };
 
 mount(express());
