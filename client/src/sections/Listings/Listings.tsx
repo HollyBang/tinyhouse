39021f3abc9dfcc110b325436c193f8ec02e6bfe
HollyBang <mazafakand@gmail.com>
2020-04-05 22:59:53 +0300

    added delete listing method, types, generic for fetch

--- client/src/sections/Listings/Listings.tsx
+++ client/src/sections/Listings/Listings.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from 'react';
 import { server } from '../../lib/api';
-import { ListingsData } from './types';
+import { ListingsData, DeleteListingData, DeleteListingVariables } from './types';
 
 const LISTINGS = `
   query Listings {
@@ -18,6 +18,14 @@ const LISTINGS = `
   }
 `;
 
+const DELETE_LISTING = `
+  mutation DeleteListing($id: ID!) {
+    deleteListing(id: $id) {
+      id
+    }
+  }
+`;
+
 interface Props {
   title: string;
 }
@@ -28,10 +36,22 @@ export const Listings = ({ title }: Props) => {
     console.log(data);
   };
 
+  const deleteListings = async () => {
+    const { data } = await server.fetch<DeleteListingData, DeleteListingVariables>({
+      query: DELETE_LISTING,
+      variables: {
+        id: '5e8252d251e4bd24ac2a7a56',
+      },
+    });
+
+    console.log(data);
+  };
+
   return (
     <div>
       <h2>{title}</h2>
       <button onClick={fetchListings}>Query Listings!</button>
+      <button onClick={deleteListings}>Delete Listings!</button>
     </div>
   );
 };
